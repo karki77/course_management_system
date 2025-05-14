94274cef0588f764cf4bf541bc69f9d3fbc5b342
Kuber Karki <karkikuber5@gmail.com>
2025-05-14 12:27:59 +0545

    Add .gitattributes to handle line endings

---
+++ src/utils/email/email.ts
@@ -0,0 +1,25 @@
+import HttpException from "#utils/api/httpException";
+import nodemailer from "nodemailer";
+const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: process.env.GMAIL_USER,
+        pass: process.env.GMAIL_PASS,
+    },
+});
+
+export const sendVerificationEmail = async (email: string, verificationLink: string) => {   
+    const mailOptions = {
+        from: process.env.EMAIL_USER,
+        to: email,
+        subject: "Email Verification",
+        text: `Please verify your email by clicking on the following link: ${verificationLink}`,
+    };
+
+    try {
+        await transporter.sendMail(mailOptions);
+        throw new HttpException(200, "Verification email sent successfully.");
+    } catch (error) {
+        throw new HttpException(500, "Failed to send verification email.");
+    }
+}
--- src/utils/email/template.ts
+++
@@ -1,8 +0,0 @@
-/**
- * REGISTER, USER EMAIL
- */
-export const registerEmailTemplate = () => {
-  return `
-        // email -> html template.
-    `;
-};
